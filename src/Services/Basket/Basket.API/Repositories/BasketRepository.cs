01227e7358ab8eab576600df37e3753c49ff0139
Ur1m <106539236+UrimB@users.noreply.github.com>
2022-11-25 16:45:13 +0100

    Create second microservice with redis connection

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,41 @@
+﻿using Basket.API.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache redisCache)
+        {
+            _redisCache = redisCache ?? throw new ArgumentNullException(nameof(redisCache));
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (String.IsNullOrEmpty(basket))
+            {
+                return null;
+            }
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+
+        public Task Delete(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+     
+        public Task<ShoppingCart> Update(ShoppingCart basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
